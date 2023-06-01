3703ca0b2bb21d6bc178a1409693e0f1ead879d6
GBATZOLIS <gb511@cam.ac.uk>
2023-06-01 17:04:44 +0100

    pytorch lightning support

--- guided_diffusion/scoreVAE_pl_module.py
+++ guided_diffusion/scoreVAE_pl_module.py
@@ -23,34 +23,127 @@ from guided_diffusion.script_util import (
     encoder_defaults,
     create_encoder
 )
+from guided_diffusion.resample import create_named_schedule_sampler
+from pytorch_lightning.callbacks import Callback
+import torch.optim as optim
+from torch.optim import AdamW
 
 class ScoreVAE(pl.LightningModule):
     def __init__(self, args):
         super(ScoreVAE, self).__init__()
+        self.save_hyperparameters()
+        
+        self.args = args
 
         self.encoder = create_encoder(
         **args_to_dict(args, encoder_defaults().keys()))
 
         self.diffusion_model, self.diffusion = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
-    )
-
-    def forward(self, x):
-        # Define your forward pass.
-        x = F.relu(self.layer1(x))
-        x = F.softmax(self.layer2(x), dim=1)
-        return x
+        )
+        self.schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, self.diffusion)
 
     def training_step(self, batch, batch_idx):
         # training_step defined the train loop.
         # It is independent of forward
-        x, y = batch
-        y_hat = self(x)
-        loss = F.cross_entropy(y_hat, y)
-        self.log('train_loss', loss)
+        x, cond = batch
+        t, weights = self.schedule_sampler.sample(x.shape[0], x.device)
+
+        compute_losses = functools.partial(
+                self.diffusion.scoreVAE_training_losses,
+                self.encoder,
+                self.diffusion_model,
+                x,
+                t,
+                model_kwargs=cond,
+                train=True
+            )
+
+        losses = compute_losses()
+        if isinstance(self.schedule_sampler, LossAwareSampler):
+                self.schedule_sampler.update_with_local_losses(
+                    t, losses["loss"].detach()
+                )
+
+        loss = (losses["loss"] * weights).mean()
+        self.log('train_loss', loss, prog_bar=True)
+        self.log_loss_dict(self.diffusion, t, {k: v * weights for k, v in losses.items()})
+
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        # training_step defined the train loop.
+        # It is independent of forward
+        x, cond = batch
+        t, weights = self.schedule_sampler.sample(x.shape[0], x.device)
+
+        compute_losses = functools.partial(
+                self.diffusion.scoreVAE_training_losses,
+                self.encoder,
+                self.diffusion_model,
+                x,
+                t,
+                model_kwargs=cond,
+                train=False
+            )
+
+        losses = compute_losses()
+        if isinstance(self.schedule_sampler, LossAwareSampler):
+                self.schedule_sampler.update_with_local_losses(
+                    t, losses["loss"].detach()
+                )
+
+        loss = (losses["loss"] * weights).mean()
+        self.log('val_loss', loss, prog_bar=True)
+        #self.log_loss_dict(self.diffusion, t, {k: v * weights for k, v in losses.items()})
         return loss
 
     def configure_optimizers(self):
-        # Configure your optimizers and learning rate schedulers
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
-        return optimizer
+        class scheduler_lambda_function:
+            def __init__(self, warm_up):
+                self.use_warm_up = True if warm_up > 0 else False
+                self.warm_up = warm_up
+
+            def __call__(self, s):
+                if self.use_warm_up:
+                    if s < self.warm_up:
+                        return s / self.warm_up
+                    else:
+                        return 1
+                else:
+                    return 1
+        
+
+        optimizer = AdamW(self.encoder.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
+        
+        scheduler = {'scheduler': optim.lr_scheduler.LambdaLR(optimizer,scheduler_lambda_function(self.args.lr_anneal_steps)),
+                    'interval': 'step'}  # called after each training step
+                    
+        return [optimizer], [scheduler]
+
+    #logging helper
+    def log_loss_dict(self, diffusion, ts, losses):
+        for key, values in losses.items():
+            self.log(key, values.mean())
+            # Log the quantiles (four quartiles, in particular).
+            for sub_t, sub_loss in zip(ts.cpu().detach().numpy(), values.detach().cpu().numpy()):
+                quartile = int(4 * sub_t / diffusion.num_timesteps)
+                self.log(f"{key}_q{quartile}", sub_loss)
+
+class LoadAndFreezeModelCallback(Callback):
+    def __init__(self, args):
+        super().__init__()
+        self.diffusion_model_checkpoint = args.diffusion_model_checkpoint
+
+    def on_train_start(self, trainer, pl_module):
+        if trainer.global_rank == 0:
+            trainer.logger.info(f"Loading pretrained score model from checkpoint: {self.diffusion_model_checkpoint}...")
+            pl_module.diffusion_model.load_state_dict(
+                th.load(
+                    self.diffusion_model_checkpoint, map_location=lambda storage, loc: storage.cuda(trainer.root_device)
+                )
+            )
+
+        # Freeze the unconditional score model
+        for param in pl_module.diffusion_model.parameters():
+            param.requires_grad = False
---
+++ scripts/scoreVAE_pl_train.py
@@ -0,0 +1,114 @@
+"""
+Train a diffusion model on images.
+"""
+
+import argparse
+
+from guided_diffusion.pl_image_datasets import ImageDataModule
+from guided_diffusion.resample import create_named_schedule_sampler
+from guided_diffusion.script_util import (
+    model_and_diffusion_defaults,
+    create_model_and_diffusion,
+    args_to_dict,
+    add_dict_to_argparser,
+    encoder_defaults,
+    create_encoder
+)
+from guided_diffusion.train_scoreVAE_util import TrainLoop
+from guided_diffusion.pl_ema import EMA
+from guided_diffusion.scoreVAE_pl_module import ScoreVAE, LoadAndFreezeModelCallback
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
+
+import psutil
+
+
+def print_memory_usage():
+    mem_info = psutil.virtual_memory()
+    print(f'Used memory: {mem_info.used / 1024**3:.2f} GB')
+    print(f'Total memory: {mem_info.total / 1024**3:.2f} GB')
+    print(f'Free memory: {mem_info.free / 1024**3:.2f} GB')
+    print(f'Memory percentage used: {mem_info.percent}%')
+
+def main():
+    args = create_argparser().parse_args()
+
+    #-----dataset-----
+    print("memory report before loading the dataset in RAM...")
+    print_memory_usage()
+    datamodule = ImageDataModule(data_dir=args.data_dir,
+                                batch_size=args.batch_size,
+                                image_size=args.image_size,
+                                class_cond=args.class_cond,
+                                num_workers=args.workers)
+    
+    print("memory report after loading the dataset in RAM...")
+    print_memory_usage()
+    datamodule.setup()
+    #-----------------
+
+    print("training...")
+    logger = pl.loggers.TensorBoardLogger(args.logdir, name='', version=args.log_name)
+
+    callbacks = [EMA(decay=args.ema_rate), LoadAndFreezeModelCallback(args)]
+    trainer = pl.Trainer(gpus=args.gpus,
+                          num_nodes = args.num_nodes,
+                          accelerator = args.accelerator,
+                          accumulate_grad_batches = args.accumulate_grad_batches,
+                          gradient_clip_val = args.grad_clip,
+                          max_steps=args.step_limit, 
+                          max_epochs =args.epochs_limit,
+                          callbacks=callbacks, 
+                          logger = logger,
+                          resume_from_checkpoint=args.resume_checkpoint
+                          )
+
+    LightningModule = ScoreVAE(args)
+    trainer.fit(LightningModule, datamodule=datamodule)
+
+def create_argparser():
+    defaults = dict(
+        data_dir="",
+        log_dir="", 
+        log_name="",
+        schedule_sampler="uniform",
+        lr=1e-4,
+        weight_decay=0.0,
+        lr_anneal_steps=0,
+        gpus=1, #new
+        nodes=1, #new
+        accelerator = None, #new
+        workers = 4, #new
+        accumulate_grad_batches = False, #new
+        grad_clip = 0., #new 
+        batch_size=1,
+        microbatch=-1,  # -1 disables microbatches
+        ema_rate="0.9999",  # comma-separated list of EMA values
+        log_interval=10,
+        save_interval=10000,
+        resume_checkpoint=None,
+        step_limit = 2000000, #new
+        epochs_limit = 10000, #new
+        use_fp16=False,
+        fp16_scale_growth=1e-3,
+        
+        ### ScoreVAE settings #new
+        diffusion_model_checkpoint="",
+        latent_dim = 1024, 
+        encoder_use_fp16 = False,
+        encoder_width = 128,
+        encoder_depth = 2,
+        encoder_attention_resolutions = "32,16,8",
+        encoder_use_scale_shift_norm = True,
+        encoder_resblock_updown = True,
+        encoder_pool = "attention"
+    )
+    defaults.update(model_and_diffusion_defaults())
+    parser = argparse.ArgumentParser()
+    add_dict_to_argparser(parser, defaults)
+
+    return parser
+
+
+if __name__ == "__main__":
+    main()
