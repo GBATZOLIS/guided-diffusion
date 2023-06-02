8d62b763f962e16c9f1a06521f01411d90b3a461
GBATZOLIS <gb511@cam.ac.uk>
2023-06-02 01:46:04 +0100

    mpi is not need in the new code

--- guided_diffusion/scoreVAE_pl_module.py
+++ guided_diffusion/scoreVAE_pl_module.py
@@ -8,7 +8,6 @@ import torch.distributed as dist
 from torch.nn.parallel.distributed import DistributedDataParallel as DDP
 from torch.optim import AdamW
 
-from . import dist_util, logger
 from .fp16_util import MixedPrecisionTrainer
 from .nn import update_ema
 from .resample import LossAwareSampler, UniformSampler
