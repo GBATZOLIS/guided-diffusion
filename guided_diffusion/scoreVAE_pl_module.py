195de55894bd21fe87bcaa9479707dc4ea8f83c7
GBATZOLIS <gb511@cam.ac.uk>
2023-06-04 23:07:51 +0100

    mm

--- guided_diffusion/scoreVAE_pl_module.py
+++ guided_diffusion/scoreVAE_pl_module.py
@@ -66,8 +66,7 @@ class ScoreVAE(pl.LightningModule):
             param.requires_grad = False
         
         #self.diffusion_model = torch.compile(self.diffusion_model)
-        self.diffusion_model.convert_to_fp16()
-        
+        self.diffusion_model = self.diffusion_model.half()        
 
     def training_step(self, batch, batch_idx):
         # training_step defined the train loop.
